e5410df5fd354158d204c6dd777fed3fe8af944a
Muhammet Yildiz <muhammetyildiz@gmail.com>
2020-04-11 19:10:33 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,9 +1,7 @@
 count = 9
 magic_number = 5
 while count > 0 do
-  break if count == magic_number
-    puts "sorry"
-  puts "My count is #{count}, keep counting down"
+  puts count
   count -= 1
 end
-puts "My count is now 0, We are done counting down"
+puts "happy New Year!"
